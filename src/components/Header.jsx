2daf292eb52e1fc221a3799e59b2b6842f1b532e
Faran <faran.7j99@gmail.com>
2025-01-25 08:27:02 +0330

    make header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="header">
+                <h2>Posts</h2>
+            </div>
+        </header>
+    );
+}
